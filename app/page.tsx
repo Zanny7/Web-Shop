a0ea939a840399bcc36317d08b63e630c14fc30e
JessicaFredin <110724442+JessicaFredin@users.noreply.github.com>
2026-02-23 14:56:50 +0100

    fix: pagination and navigation best practise styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,55 @@
 import { Suspense } from "react";
 import SearchBar from "@/components/search/search-bar";
 import type { ProductsResponse } from "@/types/product-response";
+import Table from "@/components/table";
 
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
 export default async function Home({
-  searchParams,
+	searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const { q } = await searchParams;
+	const { q } = await searchParams;
+	const { page: urlPage } = await searchParams;
 
-  const searchQuery = q && typeof q === "string" ? `&q=${q}` : "";
+	const searchQuery = q && typeof q === "string" ? `&q=${q}` : "";
+	const currentPage =
+		urlPage && typeof urlPage === "string" ? Number(urlPage) : 1;
 
-  // we use the fetch() method to get the products from the API
-  // in this fetch we sort using _sort and _order and we limit the number of products using _limit
-  // we also use _expand to get the relational category data
-  // we can use the other destructed variables like page, total and so on to create pagination or show info
+	// we use the fetch() method to get the products from the API
+	// in this fetch we sort using _sort and _order and we limit the number of products using _limit
+	// we also use _expand to get the relational category data
+	// we can use the other destructed variables like page, total and so on to create pagination or show info
 
-  const { products, total, page, pages, limit }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category${searchQuery}`,
-  ).then((res) => res.json());
+	const { products, total, page, pages, limit }: ProductsResponse =
+		await fetch(
+			`${API_URL}/products/?_limit=${defaultLimit}&_page=${currentPage}&_sort=id&_order=desc&_expand=category${searchQuery}`,
+		).then((res) => res.json());
 
-  console.log(products);
+	console.log(products);
 
-  return (
-    <main>
-      <h1>Products</h1>
-      <Suspense>
-        <SearchBar />
-      </Suspense>
-      <div>
-        {products.map((product) => (
-          <h2 key={product.id}>
-            {product.title} - {product.category?.name}
-          </h2>
-        ))}
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<h1>Products</h1>
+			<Suspense>
+				<SearchBar />
+			</Suspense>
+			<div>
+				{products.map((product) => (
+					<h2 key={product.id}>
+						{product.title} - {product.category?.name}
+					</h2>
+				))}
+			</div>
+
+			<Table
+				products={products}
+				total={total}
+				page={currentPage}
+				pages={pages}
+			/>
+		</main>
+	);
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -28,58 +28,81 @@ export default function Navigation() {
         { name: "Settings", icon: <Settings size={20} />, href: "" },
     ];
 
-    return(
-        <>
-            {/* Hamburger Menu */}
-            <div ref={menuRef} className="absolute m-2">
-                <button className="block md:hidden cursor-pointer p-1 focus:bg-black focus:text-white focus:rounded-sm" type="button" onClick={() => setIsOpen(!isOpen)}>
-                    <Menu size={28}/>
-                </button>
+    return (
+		<>
+			{/* Hamburger Menu */}
+			<div ref={menuRef} className="absolute m-2">
+				<button
+					className="block md:hidden cursor-pointer p-1 focus:bg-black focus:text-white focus:rounded-sm"
+					type="button"
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<Menu size={28} />
+				</button>
 
-                {isOpen && (
-                    <div className="relative top-2 -left-3 border-r border-y bg-white shadow-lg rounded-md p-4">
-                        <ul>
-                            {menuItems.map((item) =>
-                            <li key= {item.name}>
-                                <Link href={item.href} className="flex items-center py-1 px-2 rounded-md focus:outline-none focus:bg-[rgb(117,73,148)] focus:text-white">
-                                    <span className="mr-3">{item.icon}</span>
-                                    <span className="text-xl">{item.name}</span>
-                                </Link>
-                            </li>)}
-                        </ul>
-                    </div>
-                )}
-            </div>
+				{isOpen && (
+					<div className="relative top-2 -left-3 border-r border-y bg-white shadow-lg rounded-md p-4">
+						<ul>
+							{menuItems.map((item) => (
+								<li key={item.name}>
+									<Link
+										href={item.href}
+										className="flex items-center py-1 px-2 rounded-md focus:outline-none focus:bg-[rgb(117,73,148)] focus:text-white"
+									>
+										<span className="mr-3">
+											{item.icon}
+										</span>
+										<span className="text-xl">
+											{item.name}
+										</span>
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
 
-            {/* Normal Navigation */}
-            <nav className="hidden md:flex flex-col w-[200px] min-h-screen border-r border-gray-300">
-                <header className="m-5 flex flex-col">
-                    <h1 className="text-2xl font-bold">Webbutiken</h1>
-                    <span className=" text-md text-black/50">Admin panel</span>
+			{/* Normal Navigation */}
+            <nav className="hidden md:flex flex-col w-50 min-h-screen border-r border-gray-300">
+                {/* Added border and padding instead of margin and a div for the line */}
+				<header className="p-5 flex flex-col border-b border-gray-300">
+					<h1 className="text-2xl font-bold">Webbutiken</h1>
+					<span className=" text-md text-black/50">Admin panel</span>
                 </header>
-                <div className="w-full h-[1px] bg-gray-300" />
-                <ul className="p-5 flex flex-col gap-3 flex-1">
-                    {menuItems.map((item) =>
-                        <li key= {item.name}>
-                            <Link href={item.href} className="flex items-center py-1 px-2 rounded-md focus:outline-none focus:bg-[rgb(117,73,148)] focus:text-white">
-                                <span className="mr-3">{item.icon}</span>
-                                <span className="text-xl">{item.name}</span>
-                            </Link>
-                        </li>)}
-                </ul>
-                <div className="w-[100%] h-[1px] bg-gray-300"/>
-                <footer className="mr-5 my-5 ml-3 flex gap-3">
-                    <div className="flex my-auto ring-1 rounded-full">
-                        <User size={40} className=""/>
-                    </div>
-                    <div className="flex flex-col">
-                        <span className="text-lg h-6">Admin User</span>
-                        <span className="text-xs text-black/70">admin@webbutiken.se</span>
-                    </div>
-                </footer>
-            </nav>
-        </>
-    );
+                
+                {/* <div className="w-full h-px bg-gray-300" /> */}
+                
+				<ul className="p-5 flex flex-col gap-3 flex-1">
+					{menuItems.map((item) => (
+						<li key={item.name}>
+							<Link
+								href={item.href}
+								className="flex items-center py-1 px-2 rounded-md focus:outline-none focus:bg-[rgb(117,73,148)] focus:text-white"
+							>
+								<span className="mr-3">{item.icon}</span>
+								<span className="text-xl">{item.name}</span>
+							</Link>
+						</li>
+					))}
+				</ul>
+				{/* <div className="w-full h-px bg-gray-300"/> */}
+
+				{/* Added border and padding instead of a div and margin */}
+				<footer className="py-5 px-3 flex gap-3 border-t border-gray-300">
+					<div className="flex my-auto ring-1 rounded-full">
+						<User size={40} className="" />
+					</div>
+					<div className="flex flex-col">
+						<span className="text-lg h-6">Admin User</span>
+						<span className="text-xs text-black/70">
+							admin@webbutiken.se
+						</span>
+					</div>
+				</footer>
+			</nav>
+		</>
+	);
 }
 
 
